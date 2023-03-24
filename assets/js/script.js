e551cfd22a5eb93c01c9785a54a30208e0ba214f
L0rdGabbers <gabrielmarriage@me.com>
2023-03-24 14:36:20 +0000

    Adds number input bar to bet div

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+//Waits for the DOM to finish loading before beginning the first round
+//Gets the bets input element and adds an event listener to it
+function beginRound() {
+
+}
+
+function endRound() {
+
+}
+
+function dealWholeTable() {
+
+}
+
+function hit() {
+
+}
+
+function stand() {
+
+}
+
+function split() {
+
+}
+
+function doubleDown() {
+
+}
+

3da220f38b7ed1241b3a0c0d008e8a330ce22e1e
L0rdGabbers <gabrielmarriage@me.com>
2023-03-25 18:56:26 +0000

    Adds feature so that bets cannot be placed after first bet is made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 //Waits for the DOM to finish loading before beginning the first round
-//Gets the bets input element and adds an event listener to it
+//Gets the bet's input element and adds an event listener to it
+//Checks to ensure player's bet is valid and if valid
+//will remove player's bet from winnings
 document.addEventListener("DOMContentLoaded", function() {
     let bet = document.getElementById('bet');
-    bet.addEventListener('keydown', function(event) {
+    bet.addEventListener('keypress', function collectBets(event) {
         if (event.key === "Enter") {
             if (this.valueAsNumber === 0) {
                 alert("You gotta pay to play round 'ere")
@@ -11,17 +13,17 @@ document.addEventListener("DOMContentLoaded", function() {
             } else if (this.valueAsNumber > parseFloat(document.getElementById('score').innerHTML)) {
                 alert("I'm sorry kiddo, we don't accept pretend money here...")
             } else {
-                alert("Collecting bets")
+                alert("Collecting bets ...")
                 let subtractBet = (this.valueAsNumber);
                 let oldScore = parseFloat(document.getElementById('score').innerHTML);
                 let newScore = oldScore - subtractBet;
                 document.getElementById('score').innerHTML = newScore;
                 document.getElementById('bet').setAttribute("max", newScore);
+                document.getElementById('bet').removeEventListener('keypress', collectBets)
                 beginRound();
             }
         }
     })
-
 })
 
 

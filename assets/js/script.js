876a152b8fae3a5c55111af46195acdbe660b46e
L0rdGabbers <gabrielmarriage@me.com>
2023-03-27 21:07:32 +0000

    Converts array of strings into an array of objects for card numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,37 +31,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 function beginRound() {
-    let cardImgs = [`<img src="./assets/images/2oc-card.png" data-type="2" class="card" Alt="Two of Clubs">`, `<img src="./assets/images/2od-card.png" data-type="2" class="card" Alt="Two of Diamonds">`,
-    `<img src="./assets/images/2oh-card.png" data-type="2" class="card" Alt="Two of Hearts">`, `<img src="./assets/images/2os-card.png" data-type="2" class="card" Alt="Two of Spades">`,
-    `<img src="./assets/images/3oc-card.png" data-type="3" class="card" Alt="Three of Clubs">`, `<img src="./assets/images/3od-card.png" data-type="3" class="card" Alt="Three of Diamonds">`,
-    `<img src="./assets/images/3oh-card.png" data-type="3" class="card" Alt="Three of Hearts">`, `<img src="./assets/images/2os-card.png" data-type="3" class="card" Alt="Three of Spades">`, 
-    `<img src="./assets/images/4oc-card.png" data-type="4" class="card" Alt="Four of Clubs">`, `<img src="./assets/images/4od-card.png" data-type="4" class="card" Alt="Four of Diamonds">`,
-    `<img src="./assets/images/2oh-card.png" data-type="4" class="card" Alt="Four of Hearts">`, `<img src="./assets/images/2os-card.png" data-type="4" class="card" Alt="Four of Spades">`,
-    `<img src="./assets/images/5oc-card.png" data-type="5" class="card" Alt="Five of Clubs">`, `<img src="./assets/images/5od-card.png" data-type="5" class="card" Alt="Five of Diamonds">`,
-    `<img src="./assets/images/5oh-card.png" data-type="5" class="card" Alt="Five of Hearts">`, `<img src="./assets/images/5os-card.png" data-type="5" class="card" Alt="Five of Spades">`, 
-    `<img src="./assets/images/6oc-card.png" data-type="6" class="card" Alt="Six of Clubs">`, `<img src="./assets/images/6od-card.png" data-type="6" class="card" Alt="Six of Diamonds">`,
-    `<img src="./assets/images/6oh-card.png" data-type="6" class="card" Alt="Six of Hearts">`, `<img src="./assets/images/6os-card.png" data-type="6" class="card" Alt="Six of Spades">`, 
-    `<img src="./assets/images/7oc-card.png" data-type="7" class="card" Alt="Seven of Clubs">`, `<img src="./assets/images/7od-card.png" data-type="7" class="card" Alt="Seven of Diamonds">`,
-    `<img src="./assets/images/7oh-card.png" data-type="7" class="card" Alt="Seven of Hearts">`, `<img src="./assets/images/7os-card.png" data-type="7" class="card" Alt="Seven of Spades">`, 
-    `<img src="./assets/images/8oc-card.png" data-type="8" class="card" Alt="Eight of Clubs">`, `<img src="./assets/images/8od-card.png" data-type="8" class="card" Alt="Eight of Diamonds">`,
-    `<img src="./assets/images/8oh-card.png" data-type="8" class="card" Alt="Eight of Hearts">`, `<img src="./assets/images/8os-card.png" data-type="8" class="card" Alt="Eight of Spades">`, 
-    `<img src="./assets/images/9oc-card.png" data-type="9" class="card" Alt="Nine of Clubs">`, `<img src="./assets/images/9od-card.png" data-type="9" class="card" Alt="Nine of Diamonds">`,
-    `<img src="./assets/images/9oh-card.png" data-type="9" class="card" Alt="Nine of Hearts">`, `<img src="./assets/images/9os-card.png" data-type="9" class="card" Alt="Nine of Spades">`, 
-    `<img src="./assets/images/10oc-card.png" data-type="10" class="card" Alt="Ten of Clubs">`, `<img src="./assets/images/10od-card.png" data-type="10" class="card" Alt="Ten of Diamonds">`,
-    `<img src="./assets/images/10oh-card.png" data-type="10" class="card" Alt="Ten of Hearts">`, `<img src="./assets/images/10os-card.png" data-type="10" class="card" Alt="Ten of Spades">`, 
-    `<img src="./assets/images/joc-card.png" data-type="10" class="card" Alt="Jack of Clubs">`, `<img src="./assets/images/jod-card.png" data-type="10" class="card" Alt="Jack of Diamonds">`,
-    `<img src="./assets/images/joh-card.png" data-type="10" class="card" Alt="Jack of Hearts">`, `<img src="./assets/images/jos-card.png" data-type="10" class="card" Alt="Jack of Spades">`, 
-    `<img src="./assets/images/qoc-card.png" data-type="10" class="card" Alt="Queen of Clubs">`, `<img src="./assets/images/qod-card.png" data-type="10" class="card" Alt="Queen of Diamonds">`,
-    `<img src="./assets/images/qoh-card.png" data-type="10" class="card" Alt="Queen of Hearts">`, `<img src="./assets/images/qos-card.png" data-type="10" class="card" Alt="Queen of Spades">`, 
-    `<img src="./assets/images/koc-card.png" data-type="10" class="card" Alt="King of Clubs">`, `<img src="./assets/images/kod-card.png" data-type="10" class="card" Alt="King of Diamonds">`,
-    `<img src="./assets/images/koh-card.png" data-type="10" class="card" Alt="King of Hearts">`, `<img src="./assets/images/kos-card.png" data-type="10" class="card" Alt="King of Spades">`, 
-    `<img src="./assets/images/aoc-card.png" data-type="11" class="card" Alt="Ace of Clubs">`, `<img src="./assets/images/aod-card.png" data-type="11" class="card" Alt="Ace of Diamonds">`,
-    `<img src="./assets/images/aoh-card.png" data-type="11" class="card" Alt="Ace of Hearts">`, `<img src="./assets/images/aos-card.png" data-type="11" class="card" Alt="Ace of Spades">`,  ]
+    let cardImgs = [{source: "./assets/images/2oc-card.png", card: "Two of Clubs", points: 2}, {source: "./assets/images/2od-card.png", card: "Two of Diamonds", points: 2},
+    {source: "./assets/images/2oh-card.png", card: "Two of Hearts", points: 2}, {source: "./assets/images/2os-card.png", card: "Two of Spades", points: 2},
+    {source: "./assets/images/3oc-card.png", card: "Three of Clubs", points: 3}, {source: "./assets/images/3od-card.png", card: "Three of Diamonds", points: 3},
+    {source: "./assets/images/3oh-card.png", card: "Three of Hearts", points: 3}, {source: "./assets/images/3os-card.png", card: "Three of Spades", points: 3}, 
+    {source: "./assets/images/4oc-card.png", card: "Four of Clubs", points: 4}, {source: "./assets/images/4od-card.png", card: "Four of Diamonds", points: 4},
+    {source: "./assets/images/4oh-card.png", card: "Four of Hearts", points: 4}, {source: "./assets/images/4os-card.png", card: "Four of Spades", points: 4},
+    {source: "./assets/images/5oc-card.png", card: "Five of Clubs", points: 5}, {source: "./assets/images/5od-card.png", card: "Five of Diamonds", points: 5},
+    {source: "./assets/images/5oh-card.png", card: "Five of Hearts", points: 5}, {source: "./assets/images/5os-card.png", card: "Five of Spades", points: 5},, 
+    {source: "./assets/images/6oc-card.png", card: "Six of Clubs", points: 6}, {source: "./assets/images/6od-card.png", card: "Six of Diamonds", points: 6},
+    {source: "./assets/images/6oh-card.png", card: "Six of Hearts", points: 6}, {source: "./assets/images/6os-card.png", card: "Six of Spades", points: 6},, 
+    {source: "./assets/images/7oc-card.png", card: "Seven of Clubs", points: 7}, {source: "./assets/images/7od-card.png", card: "Seven of Diamonds", points: 7},
+    {source: "./assets/images/7oh-card.png", card: "Seven of Hearts", points: 7}, {source: "./assets/images/7os-card.png", card: "Seven of Spades", points: 7}, 
+    {source: "./assets/images/8oc-card.png", card: "Eight of Clubs", points: 8}, {source: "./assets/images/8od-card.png", card: "Eight of Diamonds", points: 8},
+    {source: "./assets/images/8oh-card.png", card: "Eight of Hearts", points: 8}, {source: "./assets/images/8os-card.png", card: "Eight of Spades", points: 8}, 
+    {source: "./assets/images/9oc-card.png", card: "Nine of Clubs", points: 9}, {source: "./assets/images/9od-card.png", card: "Nine of Diamonds", points: 9},
+    {source: "./assets/images/9oh-card.png", card: "Nine of Hearts", points: 9}, {source: "./assets/images/9os-card.png", card: "Nine of Spades", points: 9}, 
+    {source: "./assets/images/10oc-card.png", card: "Ten of Clubs", points: 10}, {source: "./assets/images/10od-card.png", card: "Ten of Diamonds", points: 10},
+    {source: "./assets/images/10oh-card.png", card: "Ten of Hearts", points: 10}, {source: "./assets/images/10os-card.png", card: "Ten of Spades", points: 10},
+    {source: "./assets/images/joc-card.png", card: "Jack of Clubs", points: 10}, {source: "./assets/images/jod-card.png", card: "Jack of Diamonds", points: 10},
+    {source: "./assets/images/joh-card.png", card: "Jack of Hearts", points: 10}, {source: "./assets/images/jos-card.png", card: "Jack of Spades", points: 10}, 
+    {source: "./assets/images/qoc-card.png", card: "Queen of Clubs", points: 10}, {source: "./assets/images/qod-card.png", card: "Queen of Diamonds", points: 10},
+    {source: "./assets/images/qoh-card.png", card: "Queen of Hearts", points: 10}, {source: "./assets/images/qos-card.png", card: "Queen of Spades", points: 10}, 
+    {source: "./assets/images/koc-card.png", card: "King of Clubs", points: 10}, {source: "./assets/images/kod-card.png", card: "King of Diamonds", points: 10},
+    {source: "./assets/images/koh-card.png", card: "King of Hearts", points: 10}, {source: "./assets/images/kos-card.png", card: "King of Spades", points: 10},
+    {source: "./assets/images/aoc-card.png", card: "Ace of Clubs", points: 11}, {source: "./assets/images/aod-card.png", card: "Ace of Diamonds", points: 11},
+    {source: "./assets/images/aoh-card.png", card: "Ace of Hearts", points: 11}, {source: "./assets/images/aos-card.png", card: "Ace of Spades", points: 11},]
     let drawCardNumber = Math.floor(Math.random() * (cardImgs.length));
     for (let i = 0; i < cardImgs.length; i++) {
         if (drawCardNumber == i) {
-            let card =
-            document.write = cardImgs[i]
             console.log(cardImgs[i]);
         }
     }
